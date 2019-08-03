d924d58d71c5d2cae35af11a3aa4c070745c4a97
tako.lee <info@sqlparser.com>
2019-08-03 12:47:25 +0800

    new my-app maven project

---
+++ my-app/src/main/java/com/gudusoft/app/App.java
@@ -0,0 +1,15 @@
+package com.gudusoft.app;
+
+/**
+ * Hello world!
+ * Javadoc for hello word project
+ * build using travis CI and github repo
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
