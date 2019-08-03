0b63e80ffbdebf87e3e7e0cf61d01b12701d1447
tako.lee <info@sqlparser.com>
2019-08-03 23:05:47 +0800

    modify javadoc app.java

--- src/main/java/com/gudusoft/app/App.java
+++ src/main/java/com/gudusoft/app/App.java
@@ -2,8 +2,10 @@ package com.gudusoft.app;
 
 /**
  * Hello world!
+ * <br>
  * Javadoc for hello word project
  * build using travis CI and github repo
+ * <ul><li>BigQuery</li><li>DB2</li><li>Informix</li></ul>
  *
  */
 public class App 
