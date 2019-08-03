6eaccd4e3424c68c8f42b27ed75ed961a896bd1e
tako.lee <info@sqlparser.com>
2019-08-03 16:03:31 +0800

    build and dist javadoc

---
+++ scripts/deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e # exit with nonzero exit code if anything fails
+
+if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]]; then
+
+echo "Starting to update gh-pages\n"
+
+#copy data we're interested in to other place
+cp -R $HOME/travis/build/sqlparser/autoJavaDoc/target/site/apidocs $HOME/dist
+
+#go to home and setup git
+cd $HOME
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis"
+
+#using token clone gh-pages branch
+git clone --quiet --branch=gh-pages https://0b2b7dd2fa9178336bf66f1284fb612b824180a9@github.com/sqlparser/autoJavaDoc.git gh-pages > /dev/null
+
+#go into directory and copy data we're interested in to that directory
+cd gh-pages
+cp -Rf $HOME/dist/* .
+
+echo "Allow files with underscore https://help.github.com/articles/files-that-start-with-an-underscore-are-missing/" > .nojekyll
+echo "[View live](https://${GH_USER}.github.io/${GH_REPO}/)" > README.md
+
+#add, commit and push files
+git add -f .
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
+git push -fq origin gh-pages > /dev/null
+
+echo "Done updating gh-pages\n"
+
+else
+ echo "Skipped updating gh-pages, because build is not triggered from the master branch."
+fi;
